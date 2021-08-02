06dbbb63c6551a313d815aff2771a74a71f286f8
ravindrakannake <ravindra_kannake@persistent.com>
2021-08-02 14:36:58 +0530

    Removed square fn and removed implementation for divide and multiply fn

--- math.py
+++ math.py
@@ -3,17 +3,10 @@ def add(x,y):
     return x+y
 #Subtract implementation
 def subtract(x,y):
-    return x-y             #on master branch
+    return x-y             
 #Multiply implementation
 def multiply(x,y):
-    return x*y             #on bug456 branch
+    pass
 #Divide implementation
 def divide(x,y):
-    if y==0:                                #on master branch
-        return DIVIDE_BY_0_ERROR
-    else:
-        return x/y
-
-#Square implementation
-def square(x):
-    return x*x
+    pass
