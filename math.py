3c953cb5f7aed5c12da0699f0de4823c6c86aea2
ravindrakannake <ravindra_kannake@persistent.com>
2021-08-02 14:57:29 +0530

    Removed subtract fn implementation

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ def add(x,y):
     return x+y
 #Subtract implementation
 def subtract(x,y):
-    return x-y
+    pass                       #on bug456 branch
 #Multiply implementation
 def multiply(x,y):
     return x*y
