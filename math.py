5c70c7053102083304f396eb44a34e63a95f87dc
ravindrakannake <ravindra_kannake@persistent.com>
2021-08-02 15:10:09 +0530

    Added square function from local repo

--- math.py
+++ math.py
@@ -9,4 +9,12 @@ def multiply(x,y):
     return x*y
 #Divide implementation
 def divide(x,y):
+<<<<<<< HEAD
     return x/y
+=======
+    pass
+
+
+def sqaure(x):
+    pass
+>>>>>>> 61c71a5 (Added square function from local repo)
